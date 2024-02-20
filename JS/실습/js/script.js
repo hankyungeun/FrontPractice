7228f02452c08fe0222e9d9a951af91cea98cb5c
hankyungeun <az2282@naver.com>
2024-02-20 17:34:26 +0900

    js 실습 문제 풀기

---
+++ JS/실습/js/script.js
@@ -0,0 +1,7 @@
+function getInfo() {
+    document.getElementById("test").innerText = 
+    "이름:" + document.getElementById("name").value + "\n\r"
+    + "아이디:" + document.getElementById("userId").value + "\n\r"
+    + "비밀번호:" + document.getElementById("userPw").value + "\n\r"
+    + "연락처:" + document.getElementById("phone").value + "\n\r"
+}
